1c039e2f70a363451ab9872fd821176228610336
Sashank Balusu <sashankbalusu@gmail.com>
2021-07-04 22:21:44 -0700

    Added local storage and stats

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// checkbox doesnt work
 let shouldCheck = 0;
 let myLibrary = [];
 let newTitle = "";
@@ -6,7 +5,10 @@ let newAuthor = "";
 let newPages = "";
 let newIsRead = "";
 let book = "";
-let inc = 0;
+let inc = myLibrary.length;
+let test = []
+let storage = "";
+let username = ""
 const container = document.querySelector("#container")
 const newBook = document.querySelector("#newbook")
 const form = document.querySelector("#form")
@@ -15,6 +17,10 @@ const author = document.querySelector("#author")
 const pages = document.querySelector("#pages")
 const read = document.querySelector("#read")
 const submit = document.querySelector("#submit")
+const yourLibrary = document.querySelector("#head")
+const yourStats = document.querySelector("#yourstats")
+
+onStart();
 submit.addEventListener("click", function (){
     newTitle = title.value;
     newAuthor = author.value;
@@ -27,25 +33,33 @@ submit.addEventListener("click", function (){
     else {
         shouldCheck = 0;
         newIsRead = false;
+        
     }
     form.setAttribute("style", "display: none;");
+    let books = document.querySelectorAll(".book");
+    books.forEach(element => {
+        element.setAttribute("style", "display: block;")
+    });
+    let stats = document.querySelector("#stats");
+    stats.setAttribute("style", "display:block;");
     book = new Book(newTitle, newAuthor, newPages, newIsRead);
     myLibrary.push(book);
+    localStorage.setItem("mylibrary", JSON.stringify(myLibrary));
     createDisplay(inc);
-    let xOut = "#close" + inc;
-    let close = document.querySelector(xOut);
-    close.addEventListener("click", function(){
-       let finalNum = close.getAttribute("id");
-       finalNum = finalNum.slice(5);
-       console.log(finalNum)
-       finalNum = "#div" + finalNum
-       let div = document.querySelector(finalNum);
-       div.remove();
-    }) 
+    closeButton(inc);
     inc++;
 })
-    
+yourLibrary.addEventListener("click", askName());
+yourStats.addEventListener("click", askName());
 newBook.addEventListener("click", addBookToLibrary);
+function askName(){
+    username = prompt("What's your name?");
+    localStorage.setItem("name", username)
+    yourLibrary.textContent = username +"'s Library"
+    yourStats.textContent = username +"'s Stats"
+
+    
+}
 function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
@@ -53,20 +67,39 @@ function Book(title, author, pages, isRead){
     this.isRead = isRead;
 
 }
-
+function closeButton(inc2) {
+    let xOut = "#close" + inc2;
+    let close = document.querySelector(xOut);
+    close.addEventListener("click", function(){
+       let finalNum = close.getAttribute("id");
+       finalNum = finalNum.slice(5);
+       myLibrary.splice(finalNum, 1);
+       localStorage.setItem("mylibrary", JSON.stringify(myLibrary));
+       console.log(finalNum)
+       finalNum = "#div" + finalNum
+       let div = document.querySelector(finalNum);
+       div.remove();
+       inc--;
+       stats();
+    }) 
+}
 function addBookToLibrary(){
     form.setAttribute("style", "display: block;");
+    let books = document.querySelectorAll(".book");
+    books.forEach(element => {
+        element.setAttribute("style", "display: none;")
+    });
+    let stats = document.querySelector("#stats");
+    stats.setAttribute("style", "display:none;")
     
 
 }
 function createDisplay(i){
-
     let bookObj = myLibrary[i];
-    console.log(bookObj);
     let div = document.createElement("div")
     div.setAttribute("id", "div" + i);
     div.classList.add("book");
-    console.log(div.getAttribute("id"));
+    div.setAttribute("style", "display: grid;")
     let title = document.createElement("h2");
     title.textContent = bookObj.title;
     let author = document.createElement("p");
@@ -78,23 +111,93 @@ function createDisplay(i){
     let id = "close" + i;
     close.setAttribute("id", id);
     close.classList.add("close")
-    
     let read = document.createElement("input");
     read.type = "checkbox";
     read.id = "readb"
     read.classList.add("readbut");
-    if (shouldCheck == 1){
+    if (bookObj.isRead == true){
         read.checked = true;
     }
+    stats();
     container.appendChild(div);
     div.appendChild(read);
     div.appendChild(close);
     div.appendChild(title);
     div.appendChild(author);
     div.appendChild(pages);
+    closeButton(i);
+
+}
+function onStart(){
+    storage = JSON.parse(localStorage.getItem("mylibrary"));
+    if ( storage != null){
+        myLibrary = storage;
+        inc = myLibrary.length;
+        for (let i = 0; i < storage.length; i++){
+            createDisplay(i);
+        }
+    }
     
 }
-//addBookToLibrary();
-//myLibrary[0] = new Book("hi", "yo", "hello", false);
-//myLibrary[1] = new Book("hi", "yo", "hello", false);
+function totPagesRead(){
+    let pagesRead = 0;
+    for (let i = 0; i < myLibrary.length; i++){
+        pagesRead += Number(myLibrary[i].pages);
+    }
+    let pagesReadHtml = document.querySelector("#pagesread");
+    pagesReadHtml.textContent = "Total pages read: " + pagesRead;
+}
+function booksRead(){
+    let counter = 0
+    for (let i = 0; i< myLibrary.length; i++){
+        if (myLibrary[i].isRead == true){
+            counter ++;
+        }
+    }
+    let booksReadHtml = document.querySelector("#booksread");
+    booksReadHtml.textContent = "Total books read: " + counter;
 
+}
+function booksNotRead(){
+    let counter = 0
+    for (let i = 0; i< myLibrary.length; i++){
+        if (myLibrary[i].isRead != true){
+            counter ++;
+        }
+    }
+    let booksNotReadHtml = document.querySelector("#booksnotread");
+    booksNotReadHtml.textContent = "Total books left to read: " + counter;
+
+}
+function totalBooks (){
+    let totalBooksHtml = document.querySelector("#totalbooks");
+    totalBooksHtml.textContent = "Total books in library: " + myLibrary.length;
+}
+function favoriteAuthor (){
+    let increment = 0;
+    let max = 0;
+    let index = 0;
+    for (let j = 0; j < myLibrary.length; j++){
+        let currAuthor = myLibrary[j].author;
+        for (let i = 0; i <myLibrary.length; i++){
+            if (myLibrary[i].author == currAuthor){
+                increment++;
+            }
+        }
+        if (increment > max){
+            max = increment;
+            index = j;
+        }
+        increment = 0;
+    }
+    favAuthorHtml = document.querySelector("#favauthor");
+    favAuthorHtml.textContent = "Your Favorite Author is: " + myLibrary[index].author;
+    
+}
+function stats(){
+    totPagesRead();
+    booksRead();
+    booksNotRead();
+    totalBooks();
+    favoriteAuthor();
+}
